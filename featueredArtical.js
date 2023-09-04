2ba410a73b0e0945ad51973bf923687690e4a85c
Ankitshreyasingh <133812978+Ankitshreyasingh@users.noreply.github.com>
2023-09-04 14:26:38 +0530

    Add files via upload

---
+++ featueredArtical.js
@@ -0,0 +1,25 @@
+ const featuredArticles = [
+    {
+      thumbnail:"https://images.pexels.com/photos/11134135/pexels-photo-11134135.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      title:"Ankit singh",
+      description:"Ankit singh.",
+      rating:"5",
+      author:"Ankit singh"
+    },
+    {
+      thumbnail:"https://images.pexels.com/photos/11134135/pexels-photo-11134135.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      title:"Ankit singh",
+      description:"Ankit singh.",
+      rating:"5",
+      author:"Ankit singh"
+    },
+    {
+      thumbnail:"https://images.pexels.com/photos/11134135/pexels-photo-11134135.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      title:"Ankit singh",
+      description:"Ankit singh.",
+      rating:"5",
+      author:"Ankit singh"
+    }
+]
+
+export default featuredArticles;
